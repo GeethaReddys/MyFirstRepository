f4a2f1cfbc4eb697533115e385e49e654b344835
student <student@achiever-83.local>
2017-12-02 14:41:15 +0530

    first  commit

---
+++ LocalRepository/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalRepository
+//
+//  Created by student on 12/2/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalRepository/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalRepository
+//
+//  Created by student on 12/2/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
